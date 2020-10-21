80eaed3d5699dd65250ce2782dd045bd54dc2cab
hiroooootaro <taro.hirotaro@icloud.com>
2020-10-21 19:51:04 +0900

    テンプレート選択画面

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,15 +2,22 @@ class OrdersController < ApplicationController
   def index
     
   end
+  def template
+    @templates = Template.all.order("created_at DESC")
+  end
 
   def show
     @templates = Template.all.order("created_at DESC")
   end
+
+  def new
+    @templates = Template.all
+  end
   
   def create
     @order = Order.new(order_params)
-
   end
+
 
   private
   def order_params
