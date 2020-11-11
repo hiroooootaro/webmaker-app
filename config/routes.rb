7270d0aca45fc75f12b5b8f18f1cb63b27bae34b
hiroooootaro <taro.hirotaro@icloud.com>
2020-11-11 17:57:14 +0900

    ルーティング　ネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:"templates#index"
-  resources :templates 
-  resources :orders do
-    collection do
-      get :template_select 
-      post :template_select 
-      get :category_select 
-      post :category_select 
+  resources :templates  do
+    resources :orders do
+      collection do
+        get :template_select 
+        post :template_select 
+      end
+
+      member do
+        get :category_select 
+        post :category_select 
+      end
     end
   end
 end
